fe37a323d58682e1281c8774bba40312c87222f1
Parth Dayal <97301303+Parth20401@users.noreply.github.com>
2024-10-02 18:52:08 +0530

    Create Single Number.py

---
+++ Array/Single Number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+
+        n = len(nums)
+        ans = 0
+        
+        for i in range(n):
+            ans ^= nums[i]
+
+        return ans
